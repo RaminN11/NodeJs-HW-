3e82e65bb30862dc4483a3e2036833018f01e9c7
Ramin <ramin_n11@icloud.com>
2024-12-17 16:28:58 +0300

    Did hw1

--- practise1/script.js
+++ practise1/script.js
@@ -1,18 +1,25 @@
-const http = require('http');
+const http = require("http");
+
+let viewCounts = {
+  home: 0,
+  about: 0,
+};
 
 const server = http.createServer((req, res) => {
     
     if (req.url === '/') {
+        viewCounts.home += 1;
         res.writeHead(200, {
             'Content-Type': 'text/html; charset= UTF-8'
         });
-        res.end('<a href = "/about">Перейти на страницу обо мне</a>');
+        res.end(` <h1>Главная страница</h1> <p>Просмотров страницы: ${viewCounts.home}</p> <a href="/about">Перейти на страницу обо мне</a>`);
         }
     else if (req.url === '/about') {
+        viewCounts.about += 1;
         res.writeHead(200, {
             'Content-Type': 'text/html; charset= UTF-8'
         });
-        res.end('<a href = "/">Перейти на главную страницу</a>');
+        res.end(`<h1>Страница "О нас"</h1> <p>Просмотров страницы: ${viewCounts.about}</p> <a href="/">Перейти на главную страницу</a>`);
         }
     else {
         res.writeHead(404, {
@@ -23,4 +30,6 @@ const server = http.createServer((req, res) => {
     
 });
 const port = '3000';
-server.listen(port);
+server.listen(port, () => {
+    console.log(`Сервер запущен на http://localhost:${port}`);
+  })
